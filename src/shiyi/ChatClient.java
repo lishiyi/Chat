c37f0ac5bb36e0c4899ba4f9d472a5fe8065947c
lishiyi <lishiyiiii@gmail.com>
2015-11-30 22:15:14 -0500

    v 1.2

--- src/shiyi/ChatClient.java
+++ src/shiyi/ChatClient.java
@@ -5,17 +5,21 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
 import java.net.*;
 import java.io.*;
 
 public class ChatClient extends Frame {
 	
+	private static final long serialVersionUID = 5037586126152572214L;
 	Socket s = null;
 	DataOutputStream dos = null;
+	DataInputStream dis = null;
+	private boolean bConnected = false;
 
 	TextField tfTxt = new TextField();
 	TextArea taContent = new TextArea();
+	
+	Thread tRecv = new Thread(new RecvThread());
 
 	public static void main(String[] args) {
 
@@ -39,12 +43,16 @@ public class ChatClient extends Frame {
 		tfTxt.addActionListener(new TFListener());
 		setVisible(true);
 		connect();
+		
+		tRecv.start();
 	}
 	
 	public void connect(){
 		try {
 			s = new Socket("127.0.0.1", 8888);
 			dos = new DataOutputStream(s.getOutputStream());
+			dis = new DataInputStream(s.getInputStream());
+			bConnected = true;
 System.out.println("connected");
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
@@ -54,13 +62,32 @@ System.out.println("connected");
 	}
 	
 	public void disconnect(){
-		try {
+		
+		try{
 			dos.close();
+			dis.close();
 			s.close();
-		} catch (IOException e) {
+		}
+		catch (IOException e){
 			e.printStackTrace();
 		}
 		
+		/*
+		try {
+			bConnected = false;
+			tRecv.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				dos.close();
+				dis.close();
+				s.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		*/
 	}
 	
 	private class TFListener implements ActionListener{
@@ -68,15 +95,33 @@ System.out.println("connected");
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			String string = tfTxt.getText().trim();
-			taContent.setText(string);
+			//taContent.setText(string);
 			tfTxt.setText("");
 			
 			try {
 				dos.writeUTF(string);
 				dos.flush();
-				//dos.close();
 			} catch (IOException e1) {
 				e1.printStackTrace();
+			}
+		}
+	}
+	
+	private class RecvThread implements Runnable{
+
+		@Override
+		public void run() {
+			try{
+				while(bConnected){
+					String str = dis.readUTF();
+					taContent.setText(taContent.getText() + str + "\n");
+				}
+			} catch (SocketException e1){
+				System.out.println("ÍË³öÁË£¬bye");
+				e1.printStackTrace();
+			}
+			catch (IOException e){
+				e.printStackTrace();
 			}
 		}
 		
--- src/shiyi/ChatServer.java
+++ src/shiyi/ChatServer.java
@@ -1,51 +1,112 @@
 package shiyi;
 import java.net.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.io.*;
 
 public class ChatServer {
 	
+	private boolean started = false;
+	private ServerSocket ss = null;
 	
+	List<Client> clients = new ArrayList<Client>();
 	
 	public static void main(String[] args) {
-		boolean started = false;
-		ServerSocket ss = null;
-		Socket s = null;
-		DataInputStream dis = null;
+		new ChatServer().start();
+	}
+	
+	public void start(){
 		try {
 			ss = new ServerSocket(8888);
+			started = true;
 		} catch(BindException e){
 			System.out.println("端口使用中。。。");
+			System.out.println("请关掉相关程序并重新运行服务器");
+			System.exit(0);
 		}
 		catch (IOException e){
 			e.printStackTrace();
 		}
 		
 		try{
-			started = true;
+			
 			while(started){
-				boolean bConnected = false;
-				s = ss.accept();
+				Socket s = ss.accept();
+				Client c = new Client(s);
 System.out.println("a client connected");
-				bConnected = true;
+				new Thread(c).start();
+				clients.add(c);
+			}
+		} catch(IOException e){
+			e.printStackTrace();
+		} finally {
+			try {
+				ss.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	
+	private class Client implements Runnable{
+		
+		private Socket s; //就是一插座
+		private DataInputStream dis = null;
+		private DataOutputStream dos = null;
+		private boolean bConnected = false;
+		
+		public Client(Socket s){
+			this.s = s;
+			try {
 				dis = new DataInputStream(s.getInputStream());
+				dos = new DataOutputStream(s.getOutputStream());
+				bConnected = true;
+			} catch (IOException e) {
+				e.printStackTrace();
+			} 
+		}
+		
+		public void send(String str){
+			try {
+				dos.writeUTF(str);
+			} catch (IOException e) {
+				System.out.println("Send failed...");
+				e.printStackTrace();
+			}
+		}
+		
+		@Override
+		public void run() {
+			try {
 				while(bConnected){
 					String str = dis.readUTF();
-					System.out.println(str);
+System.out.println(str);
+					for(int i = 0; i < clients.size(); i++){
+						Client c = clients.get(i);
+						c.send(str);
+					}
 				}
-				//dis.close();
+
+			} catch(EOFException e){
+				System.out.println("Client Closed");
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
-		} catch(EOFException e){
-			System.out.println("Client Closed");
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		finally{
-			try {
-				if(dis != null) dis.close();
-				if(s != null) s.close();
-			} catch (IOException e1) {
-				e1.printStackTrace();
+			finally{
+				try {
+					if(dis != null) dis.close();
+					if(dos != null) dos.close();
+					if(s != null) {
+						s.close();
+						s = null;
+					}
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
 			}
+			
 		}
+		
 	}
 }
